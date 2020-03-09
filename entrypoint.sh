396b31302ce3a4817de52a01615291579b257fe9
anki-code <anki-code>
2020-03-09 16:48:49 +0300

    0.4.4

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
-#while getopts f:c:v: option
-#do
-#case "${option}"
-#in
-#f) EXECUTE_FILE=${OPTARG};;
-#c) EXECUTE_COMMAND=${OPTARG};;
-#v) VERBOSE=${OPTARG};;
-#esac
-#done
+while getopts f:c:v: option
+do
+case "${option}"
+in
+f) EXECUTE_FILE=${OPTARG};;
+c) EXECUTE_COMMAND=${OPTARG};;
+v) VERBOSE=${OPTARG};;
+esac
+done
+
+if [[ $EXECUTE_COMMAND ]]; then
+  EXECUTE_COMMAND=(-c "${EXECUTE_COMMAND}")
+fi
+
+if [[ $EXECUTE_FILE ]]; then
+  EXECUTE_COMMAND=""
+fi
+
+if [[ $VERBOSE != '' ]]; then
+  export XXH_VERBOSE=$VERBOSE
+fi
 
 CURRENT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd $CURRENT_DIR
 
 # Check
 if [[ ! -f .check-done ]]; then
-  check_result=`./zsh --version 2>&1`
+  check_result=`./zsh.sh --version 2>&1`
   if [[ $check_result != *"zsh "* ]]; then
     echo "Something went wrong while running zsh on host:"
     echo $check_result
@@ -26,4 +38,4 @@ fi
 
 export XXH_HOME=`realpath $CURRENT_DIR/../../../..`
 export HISTFILE=$XXH_HOME/.zsh_history
-./zsh.sh # TODO: $EXECUTE_FILE $EXECUTE_COMMAND $VERBOSE
+./zsh.sh $EXECUTE_FILE "${EXECUTE_COMMAND[@]}"
