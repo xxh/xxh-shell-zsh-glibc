a786ba36dd0faecbce202e64a063c137a422c9be
anki-code <anki-code>
2020-03-13 00:01:45 +0300

    0.5.2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-while getopts f:c:v: option
+while getopts f:c:v:e: option
 do
 case "${option}"
 in
 f) EXECUTE_FILE=${OPTARG};;
 c) EXECUTE_COMMAND=${OPTARG};;
 v) VERBOSE=${OPTARG};;
+e) ENV+=("$OPTARG");;
 esac
 done
 
+if [[ $VERBOSE != '' ]]; then
+  export XXH_VERBOSE=$VERBOSE
+fi
+
 if [[ $EXECUTE_COMMAND ]]; then
+  if [[ $XXH_VERBOSE == '1' ]]; then
+    echo Execute command: $EXECUTE_COMMAND
+  fi
+
   EXECUTE_COMMAND=(-c "${EXECUTE_COMMAND}")
 fi
 
@@ -18,9 +27,17 @@ if [[ $EXECUTE_FILE ]]; then
   EXECUTE_COMMAND=""
 fi
 
-if [[ $VERBOSE != '' ]]; then
-  export XXH_VERBOSE=$VERBOSE
-fi
+for env in "${ENV[@]}"; do
+  name="$( cut -d '=' -f 1 <<< "$env" )";
+  val="$( cut -d '=' -f 2- <<< "$env" )";
+  val=`echo $val | base64 -d`
+
+  if [[ $XXH_VERBOSE == '1' ]]; then
+    echo Environment variable "$env": name=$name, value=$val
+  fi
+
+  export $name="$val"
+done
 
 CURRENT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd $CURRENT_DIR
